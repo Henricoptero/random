81db434d432252f04cb7cde1a0028ccd2be9f95c
henricoptero <henricoptero@icloud.com>
2015-07-07 10:22:48 +0200

    random versión 1

---
+++ random.js
@@ -0,0 +1,13 @@
+// Autor: Enrique Martínez López
+// Fecha: 07/07/2015
+
+// Math.random() devuelve número aleatorio entre 0 y 1.
+
+var numero = math.random()
+
+if (numero <= 0.5){
+  console.console.log('\n' + numero + 'menor que 0.5' + '\n');
+}
+else {
+  console.console.log('\n' + numero + 'mayor que 0.5' + '\n');
+}
